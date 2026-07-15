af89bb01bfd118825e90403634a948df6759bc5f
Zakir Motala <zakirhusseinmotala76@gmail.com>
2026-07-15 11:49:39 +0200

    feat(i18n): integrate i18n into Vue app

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import i18n from './i18n'
 
 // Register PWA Service Worker
 import { registerSW } from 'virtual:pwa-register'
 registerSW({ immediate: true })
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(i18n).mount('#app')
